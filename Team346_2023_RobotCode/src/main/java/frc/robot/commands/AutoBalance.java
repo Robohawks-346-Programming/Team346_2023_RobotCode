136a8c5800b8c1f4fa4b3aab4c30b5ea875e2988
KartikeyaGoel <99088848+KartikeyaGoel@users.noreply.github.com>
2023-03-17 20:36:53 -0400

    More efficient code for drive up

--- Team346_2023_RobotCode/src/main/java/frc/robot/commands/AutoBalance.java
+++ Team346_2023_RobotCode/src/main/java/frc/robot/commands/AutoBalance.java
@@ -11,9 +11,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 import frc.robot.commands.Drivetrain.DriveForTime;
-import frc.robot.commands.Drivetrain.JoystickDriveAutoDriveUp;
+import frc.robot.commands.Drivetrain.AutoDriveUp;
 import frc.robot.commands.Drivetrain.JoystickDriveSlowAutoBalance;
-import frc.robot.commands.Drivetrain.JoystickDriveSlowAutoCheckBalance;
 import frc.robot.commands.Intake.DeployIntakeIn;
 import frc.robot.commands.Intake.RunIntakeOut;
 
@@ -22,9 +21,8 @@ public class AutoBalance extends SequentialCommandGroup {
     public AutoBalance() {
         addCommands(
             new SequentialCommandGroup(
-                new JoystickDriveAutoDriveUp(null, 0, 0.2, 0),
-                new JoystickDriveSlowAutoBalance(null, 0, 0.1, 0),
-                new JoystickDriveSlowAutoCheckBalance(null, 0, 0.05, 0)
+                new AutoDriveUp(),
+                new JoystickDriveSlowAutoBalance(null, 0, 0.1, 0)
             )
         );
     }
--- Team346_2023_RobotCode/src/main/java/frc/robot/commands/Drivetrain/AutoDriveUp.java
+++ Team346_2023_RobotCode/src/main/java/frc/robot/commands/Drivetrain/AutoDriveUp.java
@@ -7,19 +7,15 @@ import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain.Drivetrain;
 
 /** An example command that uses an example subsystem. */
-public class JoystickDriveAutoDriveUp  extends CommandBase {
+public class AutoDriveUp  extends CommandBase {
 
   Drivetrain drivetrain;
   double x;
-double y;
-double theta;
+  double y;
+  double theta;
 
-  public JoystickDriveAutoDriveUp(Drivetrain drivetrain, double x, double y, double theta) {
-    this.drivetrain = drivetrain;
-    this.x = x;
-    this.y = y;
-    this.theta = theta;
-    addRequirements(drivetrain);
+  public AutoDriveUp() {
+    x = Constants.MAX_MOVE_VELOCITY;
   }
 
   // Called when the command is initially scheduled.
@@ -29,16 +25,9 @@ double theta;
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double doubleX = Math.abs(x) < 0.05 ? 0 : x;
-    double doubleY = Math.abs(y) < 0.05 ? 0 : y;
-    double doubleTheta = Math.abs(theta) < 0.05 ? 0 : theta;
 
-    double vx = doubleX * Constants.MAX_MOVE_VELOCITY;
-    double vy = doubleY * Constants.MAX_MOVE_VELOCITY;
-    double omega = doubleTheta * Constants.MAX_TURN_VELOCITY_SLOW;
-
-    ChassisSpeeds velocity = Constants.IS_FIELD_RELATIVE ? ChassisSpeeds.fromFieldRelativeSpeeds(vx, vy, omega, drivetrain.getHeading()) 
-      : new ChassisSpeeds(vx, vy, omega);
+    ChassisSpeeds velocity = Constants.IS_FIELD_RELATIVE ? ChassisSpeeds.fromFieldRelativeSpeeds(x, 0, 0, drivetrain.getHeading()) 
+      : new ChassisSpeeds(x, 0, 0);
 
     drivetrain.drive(velocity, true);
   }
